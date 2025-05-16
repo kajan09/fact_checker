779be945df3a2f2c7cb2863eb2bb012662e139f0
kajan09 <kai.jannsen@gmx.de>
2025-05-16 09:17:23 +0000

    whisper test functional

---
+++ whisper_test.py
@@ -0,0 +1,13 @@
+import whisper, subprocess, sys
+
+# video = sys.argv[1]        # to call like python transcribe.py test.mp4
+video = "\test.mp4"
+
+model = whisper.load_model("tiny.en")   # <tiny> multi language
+
+
+# Whisper will call ffmpeg internally – no tmp files needed
+result = model.transcribe(video, fp16=True)  # fp16 ⇒ GPU
+print(result["text"])
+
+ 
