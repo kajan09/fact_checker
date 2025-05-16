d497dcfc7e3e8131e6547ee93b5c88cb2a39e82a
kajan09 <kai.jannsen@gmx.de>
2025-05-16 09:41:17 +0000

    whisper time example

--- whisper_test.py
+++ whisper_test.py
@@ -1,10 +1,12 @@
 import whisper
+import sys
+import time
+# pip install -U openai-whisper
 
 # video = sys.argv[1]        # to call like python transcribe.py test.mp4
-video = "\test.mp4"
+video = "reel-to-wav/audio.wav"
 
-model = whisper.load_model("tiny.en")   # <tiny> multi language
-
+model = whisper.load_model("tiny")   # <tiny> multi language
 
 # Whisper will call ffmpeg internally – no tmp files needed
 result = model.transcribe(video, fp16=True)  # fp16 ⇒ GPU
